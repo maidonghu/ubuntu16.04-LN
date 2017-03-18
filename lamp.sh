8b377944887481326687f5d3f6c473c844b3ff79
maidonghu <maidonghu@users.noreply.github.com>
2017-03-17 20:12:32 -0700

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -18,6 +18,8 @@ sudo systemctl restart mysql
 
 sudo apt-get install php7.0 php-pear libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json php7.0-cgi -y
 sudo wget -O /etc/php/7.0/apache2/php.ini  https://raw.githubusercontent.com/maidonghu/ubuntu16.04-LN/master/php.ini
+sudo mkdir /var/log/php
+sudo chown www-data /var/log/php
 
 sudo mysql -sfu root < "wordpress.sql"
 rm -f wordpress.sql
