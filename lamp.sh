29f167788a6ba427a5e5d2715e1d6889ab0a915d
maidonghu <maidonghu@users.noreply.github.com>
2017-03-17 18:52:25 -0700

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -12,4 +12,12 @@ sudo a2dissite 000-default.conf
 export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get install -y -q mysql-server
 sudo mysql -sfu root < "mysql_secure_installation.sql"
+rm -f mysql_secure_installation.sql
+sudo systemctl restart mysql
+
+sudo apt-get install php7.0 php-pear libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json php7.0-cgi -y
+get /etc/php/7.0/apache2/php.ini
+
+sudo mysql -sfu root < "wordpress.sql"
+rm -f wordpress.sql
 
