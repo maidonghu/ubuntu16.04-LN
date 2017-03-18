5105c61d110321403a7c2d186617f85aa7ecf130
maidonghu <maidonghu@users.noreply.github.com>
2017-03-17 20:05:55 -0700

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -1,12 +1,12 @@
 sudo apt-get install apache2  -y
-sudo wget -o /etc/apache2/apache2.conf https://raw.githubusercontent.com/maidonghu/ubuntu16.04-LN/master/apache2.conf
-sudo wget -o /etc/apache2/mods-available/mpm_prefork.conf https://raw.githubusercontent.com/maidonghu/ubuntu16.04-LN/master/mpm_prefork.conf
+sudo wget -O /etc/apache2/apache2.conf https://raw.githubusercontent.com/maidonghu/ubuntu16.04-LN/master/apache2.conf
+sudo wget -O /etc/apache2/mods-available/mpm_prefork.conf https://raw.githubusercontent.com/maidonghu/ubuntu16.04-LN/master/mpm_prefork.conf
 sudo a2dismod mpm_event
 sudo a2enmod mpm_prefork
 sudo systemctl restart apache2
 
 sudo ufw allow "Apache Full"
-sudo wget -o /etc/apache2/sites-available/linode.mikecloud.info.conf  https://raw.githubusercontent.com/maidonghu/ubuntu16.04-LN/master/linode.mikecloud.info.conf
+sudo wget -O /etc/apache2/sites-available/linode.mikecloud.info.conf  https://raw.githubusercontent.com/maidonghu/ubuntu16.04-LN/master/linode.mikecloud.info.conf
 sudo mkdir -p /var/www/html/linode.mikecloud.info/{public_html,logs}
 sudo a2ensite linode.mikecloud.info.conf
 sudo a2dissite 000-default.conf
@@ -17,7 +17,7 @@ rm -f mysql_secure_installation.sql
 sudo systemctl restart mysql
 
 sudo apt-get install php7.0 php-pear libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json php7.0-cgi -y
-sudo wget -o /etc/php/7.0/apache2/php.ini  https://raw.githubusercontent.com/maidonghu/ubuntu16.04-LN/master/php.ini
+sudo wget -O /etc/php/7.0/apache2/php.ini  https://raw.githubusercontent.com/maidonghu/ubuntu16.04-LN/master/php.ini
 
 sudo mysql -sfu root < "wordpress.sql"
 rm -f wordpress.sql
