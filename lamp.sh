d9cb3e915dc2aceb485b9031c65bc440c3bf533d
maidonghu <maidonghu@users.noreply.github.com>
2017-03-18 01:02:15 -0700

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -10,6 +10,8 @@ sudo wget -O /etc/apache2/sites-available/linode.mikecloud.info.conf  https://ra
 sudo mkdir -p /var/www/html/linode.mikecloud.info/{public_html,logs}
 sudo a2ensite linode.mikecloud.info.conf
 sudo a2dissite 000-default.conf
+sudo systemctl reload apache2
+
 export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get install -y -q mysql-server
 sudo mysql -sfu root < "mysql_secure_installation.sql"
